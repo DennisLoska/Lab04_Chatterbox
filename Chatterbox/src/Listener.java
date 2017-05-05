3cd0cc4a93f872b8aeb1d7798539aa17e39aace8
disrupted <salomon.popp@gmail.com>
2017-05-05 13:57:03 +0200

    Listener comments

--- Chatterbox/src/Listener.java
+++ Chatterbox/src/Listener.java
@@ -1,8 +1,11 @@
 import java.io.DataInputStream;
 import java.io.IOException;
 
+/*
+* Because we want to be able to receive messages at any time we moved all of the InputStream
+* logic to a new Thread that continously runs in the background
+*/
 public class Listener extends Thread {
-  private static volatile boolean running = true;
   private DataInputStream inputStream;
 
   Listener(DataInputStream inputStream) {
@@ -14,13 +17,13 @@ public class Listener extends Thread {
     while (inputStream != null && !Thread.currentThread().isInterrupted()) {
       try {
         try {
-          System.out.println(inputStream.readUTF()); // print Server response
+          System.out.println(inputStream.readUTF()); // receives & prints Server messages
         } catch (IOException ioe) {
           ioe.printStackTrace();
           Thread.sleep(2000); // delay before retrying to avoid exhaustion
         }
       } catch (InterruptedException e) {
-        Thread.currentThread().interrupt();
+        Thread.currentThread().interrupt(); // sends signal to stop the Thread
       }
     }
   }
